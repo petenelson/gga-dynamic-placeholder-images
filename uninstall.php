cf1245d15e75fc7e68190be6ef412bd11bbde12a
Pete Nelson <pete@petenelson.com>
2015-03-08 17:28:41 -0500

    fixed syntax error in uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,7 @@
 <?php
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) wp_die( 'restricted access' );
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	wp_die( 'restricted access' );
+}
 
 // delete stats table
 global $wpdb;
@@ -7,5 +9,5 @@ $table_name = $wpdb->prefix . 'gga_dynamic_images_stats';
 $sql = "DROP TABLE IF_EXISTS $table_name";
 $wpdb->query($sql);
 
-$sql = "DELETE FROM $wpdb->options WHERE option_name like '_gga-placeholder-image-for%'"
+$sql = "DELETE FROM $wpdb->options WHERE option_name like '_gga-placeholder-image-for%'";
 $wpdb->query($sql);
